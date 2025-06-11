ecc740c9e88a91a2b4883ba306f8876d80a9a6b6
akira-erick <kajitani.motta@gmail.com>
2025-06-11 17:33:49 -0300

    feat: more words

--- words/words.py
+++ words/words.py
@@ -1,6 +1,6 @@
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), 'words_data.txt')
+file_path = os.path.join(os.path.dirname(__file__), 'output_words.txt')
 
 def get_words():
     with open(file_path, 'r') as file:
